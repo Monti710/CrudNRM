e6bf22f4402330ce2ee6c19a7ba9d1c909a0f528
IsmaelMontielHome <mayelomonti1@gmail.com>
2024-06-22 10:47:17 -0600

    create of the Token ready, create of the backend login ready, and validation of Token ready

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -18,7 +18,7 @@ export const register = async  (req, res) => {
         const token =  await createAccessToken({id: userSaved._id})
         res.cookie("token", token)
         res.json({
-            id:newUser._id,
+            id:userSaved._id,
             username:userSaved.username,
             email:userSaved.email,
             createdAt:userSaved.createdAt,
@@ -27,6 +27,50 @@ export const register = async  (req, res) => {
     } catch (error) {
         res.status(500).json({message: error.message})
     }
-}
+};
 
-export const login = (req, res) => res.send('login');
+export const login = async  (req, res) => {
+    const {email, password}  = req.body
+    
+    try {
+        const userFound = await User.findOne({email})
+
+        if(!userFound) return res.status(400).json({message: "Email or password not found incorrect"});
+
+        const isMatch = await bcrypt.compare(password, userFound.password);
+
+        if(!isMatch) return res.status(400).json({message: "Email or Password incorrect"});
+
+        const token =  await createAccessToken({id: userFound._id})
+        res.cookie("token", token)
+        res.json({
+            id:userFound._id,
+            username:userFound.username,
+            email:userFound.email,
+            createdAt:userFound.createdAt,
+            updateAt:userFound.updatedAt
+        });
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+};
+
+export const logout = (req, res) => {
+    res.cookie("token", "",{
+        expires: new Date(0)
+    });
+    return res.sendStatus(200);
+};
+
+export const profile = async (req, res) => {
+    const userFound = await  User.findById(req.user.id)
+    if(!userFound)
+        return res.status(400).json({message: "User not found"});
+    return res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updateAt: userFound.updatedAt,
+    });
+};
