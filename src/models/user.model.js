5f1c1be37190d30c436cd38ae9daacbd9f06e9de
IsmaelMontielHome <mayelomonti1@gmail.com>
2024-06-22 20:51:27 -0600

    CRUD TASK

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -22,5 +22,5 @@ const userSchema = new mongoose.Schema({
     }
 }, {
     timestamps: true,
-})
+});
 export default mongoose.model('User', userSchema)
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { authRequired} from  '../middlewares/validateToken.js';
-
+import {getTasks,getTask,createTask,updateTask,deleteTask} from '../controllers/tasks.controller.js';
 const router = Router();
 
-router.get('/tasks', authRequired, (req, res) => res.send('tasks'))
+router.get('/tasks', authRequired, getTasks ) // READ ALL
+router.post('/tasks', authRequired, createTask ) //CREATE
+router.get('/tasks/:id', authRequired, getTask ) //READ ONE
+router.put('/tasks/:id', authRequired, updateTask ) // UPDATE
+router.delete('/tasks/:id', authRequired, deleteTask ) // DELETE
 
 export default router
