9de0c467ff33ce480cc1e8a89efe453987bfd32a
IsmaelMontielHome <mayelomonti1@gmail.com>
2024-06-26 01:07:56 -0600

    Data Validation Ready

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,8 +3,11 @@ import { authRequired} from  '../middlewares/validateToken.js';
 import {getTasks,getTask,createTask,updateTask,deleteTask} from '../controllers/tasks.controller.js';
 const router = Router();
 
+import {validateSchema} from '../middlewares/validator.middleware.js'
+import {createTaskSchema} from '../schemas/task.schema.js'
+
 router.get('/tasks', authRequired, getTasks ) // READ ALL
-router.post('/tasks', authRequired, createTask ) //CREATE
+router.post('/tasks', authRequired, validateSchema(createTaskSchema), createTask ) //CREATE
 router.get('/tasks/:id', authRequired, getTask ) //READ ONE
 router.put('/tasks/:id', authRequired, updateTask ) // UPDATE
 router.delete('/tasks/:id', authRequired, deleteTask ) // DELETE
